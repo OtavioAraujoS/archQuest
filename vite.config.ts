d80c4d2c506afed3551428dfc71acd11e876a038
OtavioAraujoS <otavio.araujo@gmail.com>
2026-09-23 14:23:46 -0300

    feat: install testing library + vitest + fake-indexddb

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -9,5 +10,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+    globals: true,
+    passWithNoTests: true,
   },
 });
